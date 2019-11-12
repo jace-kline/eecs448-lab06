5ead26546ca0f227984433055172dce03e0fc583
jace-kline <jace_kline@ku.edu>
2019-11-11 22:59:48 -0600

    Created Repo and created test directory

---
+++ queue-starter/main.cpp
@@ -0,0 +1,23 @@
+/**
+*	@file main.cpp
+*	@author John Gibbons
+*	@date 2014.04.02
+*/
+
+/**
+*   Modified by: Jace Kline 2881618
+* 	Date modified: 2019.11.11
+*/
+
+#include <iostream>
+#include <stdexcept>
+#include "Queue.h"
+#include "../tests/Test.h"
+
+int main()
+{
+	Queue q;
+	Test t;
+	t.run();
+	return(0);
+}
